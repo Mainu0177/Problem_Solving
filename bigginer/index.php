1f4d6aa7d8555b59ff652e9815eb3a95cb69f221
Mainu0177 <mainuddinhassan658@gmail.com>
2025-07-27 00:26:09 +0600

    4problem solve

--- bigginer/index.php
+++ bigginer/index.php
@@ -15,18 +15,79 @@
 
 
 // 2.
-echo "Enter String: \n";
-$input = trim(fgets(STDIN));
+// echo "Enter String: \n";
+// $input = trim(fgets(STDIN));
 
-$number = '';
+// $number = '';
 
-for($i = 0; $i < strlen($input); $i++){
-    if(is_numeric($input[$i])){
-        $number = $input[$i];
-        break;
-    }
-}
+// for($i = 0; $i < strlen($input); $i++){
+//     if(is_numeric($input[$i])){
+//         $number = $input[$i];
+//         break;
+//     }
+// }
 
-echo $number . PHP_EOL;
+// echo $number . PHP_EOL;
 
 
+// 2.
+    // echo "Enter String: \n";
+    // $input = trim(fgets(STDIN));
+    // function extractNumber($input){
+    //     $number = '';
+
+    //     for($i = 0; $i < strlen($input); $i++){
+    //         if(is_numeric($input[$i])){
+    //             $number .= $input[$i];
+    //         }
+    //     }
+    //     return $number;
+    // }
+
+    // $result = extractNumber($input);
+
+    // echo $result . PHP_EOL;
+
+// 3. Write a PHP function that takes a number as input and returns the sum of all the natural numbers up to that number.
+// echo "Enter Number: \n";
+// $input = (int)trim(fgets(STDIN));
+// function sumOfNaturalNumber($input){
+//     $sum = 0;
+//     for($i = 1; $i <= $input; $i++){
+//         $sum += $i;
+//     }
+//     return $sum;
+// }
+
+// $result = sumOfNaturalNumber($input);
+// echo $result . PHP_EOL;
+
+// mathematical formula
+// function sumOfNaturalNumber($input){
+//     return ($input * ($input + 1)) / 2; // (n * (n + 1)) / 2
+// }
+// $result = sumOfNaturalNumber($input);
+// echo $result . PHP_EOL;
+
+// 4. Write a PHP function that takes a number as input and returns the remainder when divided by 2.
+// echo "Enter Number: \n";
+// $input = strtolower(trim(fgets(STDIN)));
+// function getReminderByTwo($word){
+//     $wordToNumber = [
+//         'zero' => 0,
+//         'one' => 1,
+//         'two' => 2,
+//         'three' => 3,
+//         'four' => 4,
+//         'five' => 5,
+//         'six' => 6,
+//         'seven' => 7,
+//         'eight' => 8,
+//         'nine' => 9
+//     ];
+//     if(array_key_exists($word, $wordToNumber)){
+//         $number = $wordToNumber[$word];
+//         return $number % 2;
+//     }
+// }
+// echo getReminderByTwo($input) . PHP_EOL;
